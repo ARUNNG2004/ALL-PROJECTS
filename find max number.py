af12e7ad25ddec1bab601dd18cc820878463e4f5
agilarasu <86509617+agilarasu@users.noreply.github.com>
2021-10-31 06:26:31 +0530

    Update find max number.py

--- find max number.py
+++ find max number.py
@@ -1,9 +1,9 @@
-# def max(a,b):
-#     if a>b:
-#         return a
-#     else:
-#         return b
-# x=int(input("enter the number "))
-# y=int(input("enter the number "))
-# print(max(x,y))
+def max(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+x=int(input("enter the first number "))
+y=int(input("enter the second number "))
+print(max(x,y))
 
